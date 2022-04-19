2a4a5d15a8a64996b227ad6f15e1b2fd4ad32d83
IVagrantI <jackliteng@yahoo.com>
2022-04-18 20:50:09 -0700

    fetch currency api,need to work on display fucntion

---
+++ assets/js/travel.js
@@ -0,0 +1,40 @@
+var currencyEl=document.querySelector("#currency");
+
+
+
+//create currency api function
+var getCurrency = function (){
+    //put api USD api link to a variety.
+    var apiCurrency="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json";
+    //make a request to USD api
+    fetch(apiCurrency).then(function(response){
+        console.log(response);
+        response.json().then(function(data){
+            console.log(data);
+            //japanese yen for Tokyo
+            console.log(data.usd.jpy);
+            //british pound for London
+            console.log(data.usd.gbp);
+            //Australian dollar for Sydney
+            console.log(data.usd.aud);
+        })
+    })
+}
+
+getCurrency()
+
+var displayCurrency=function(){
+    var currencyDisEL=document.createElement("p")
+    currencyDisEL.className="currency-display"
+    currencyDisEL.setAttribute=("id", "currency-display")
+    currencyDisEL.textContent="USD=6.37RMB"
+
+    //append currency display to currency section.
+    currencyEl.appendChild(currencyDisEL);
+}
+
+
+function favTutorial() {  
+    var mylist = document.getElementById("#myList");  
+    document.getElementById("#currency-display").value = mylist.options[mylist.selectedIndex].text;  
+    } 
